200ba6d94e4fde26afc3ae102e5ee9626f5f9a82
Richard Lincoln <r.w.lincoln@gmail.com>
2014-08-03 12:19:43 +0200

    Exporting more variables.

--- lib/amd.dart
+++ lib/amd.dart
@@ -1,4 +1,5 @@
 library amd;
 
 export './src/amd.dart' show NPRINT, NDEBUG, AMD_CONTROL, AMD_INFO, AMD_VERSION, AMD_VERSION_CODE,
-  AMD_MAIN_VERSION, AMD_SUB_VERSION, AMD_DATE, AMD_OK, order, defaults, control, info;
+  AMD_MAIN_VERSION, AMD_SUB_VERSION, AMD_DATE, AMD_OK, AMD_OUT_OF_MEMORY, AMD_MEMORY, AMD_LNZ,
+  AMD_NMULTSUBS_LU, AMD_NDIV, AMD_SYMMETRY, order, defaults, control, info;
